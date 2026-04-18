eaadae8a38360ff841862b5c84942e94bb5edacc
TentenWantaya <5020803@g.highlineschools.org>
2026-04-17 20:36:35 -0700

    Add Attack system

---
+++ script/enemy.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+var speed = 50
+var player_chase = false
+var player = null
+
+var health = 100
+var player_inattack_range = false
+var can_take_damage = true
+
+func _physics_process(delta):
+	deal_with_damge()
+	
+	if player_chase:
+		position += (player.position - position)/speed
+		
+		$AnimatedSprite2D.play("move")
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+		
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_range = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_range = false
+
+func deal_with_damge():
+	if player_inattack_range and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
